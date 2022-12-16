95e0df5b906827b705ed4c7ed4bb06f025ecbe34
David Haralambiev <david@davidharalambiev.tech>
2022-12-16 17:29:52 +0200

    Update Application.js

--- src/js/Application.js
+++ src/js/Application.js
@@ -10,15 +10,17 @@ export default class Application extends EventEmitter {
 
   constructor() {
     super();
-
-    const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
-    let count = 0;
-
+    this._beat=new Beat();
+    this._beat.on('event', this._create);
+    this._create=()=>{
     const message = document.createElement("div");
     message.classList.add("message");
-    message.innerText = "Ah";
-
+    message.innerText = lyrics[count];
+      count++;
     document.querySelector(".main").appendChild(message);
+    };
+    const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
+    let count = 0;
 
     this.emit(Application.events.READY);
   }
